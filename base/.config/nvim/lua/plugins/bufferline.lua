81badbd37db3b95dd7a96954eb4d4c4c26c1be0a
Chad Nickell <nickell.chad@gmail.com>
2024-12-30 14:16:16 -0500

    Bufferline tab name tweak

--- base/.config/nvim/lua/plugins/bufferline.lua
+++ base/.config/nvim/lua/plugins/bufferline.lua
@@ -1,3 +1,17 @@
+local function get_file_representation(path)
+  local relative_path = vim.fn.fnamemodify(path, ':.')
+  local basename = vim.fn.fnamemodify(path, ':t')
+
+  if relative_path:match '^[^/]+$' then
+    -- If the file is directly in the cwd, return the basename.
+    return basename
+  else
+    -- If not, return the first letter of the directory + the basename.
+    local directory = relative_path:match '^([^/]+)/'
+    return string.sub(directory, 1, 1) .. '/' .. basename
+  end
+end
+
 return {
   'akinsho/bufferline.nvim',
   version = '*',
@@ -12,6 +26,15 @@ return {
           local modified_a = vim.fn.getftime(buffer_a.path)
           local modified_b = vim.fn.getftime(buffer_b.path)
           return modified_a > modified_b
+        end,
+        name_formatter = function(buf) -- buf contains:
+          -- return buf.name
+          return get_file_representation(buf.path)
+          -- name                | str        | the basename of the active file
+          -- path                | str        | the full path of the active file
+          -- bufnr               | int        | the number of the active buffer
+          -- buffers (tabs only) | table(int) | the numbers of the buffers in the tab
+          -- tabnr (tabs only)   | int        | the "handle" of the tab, can be converted to its ordinal number using: `vim.api.nvim_tabpage_get_number(buf.tabnr)`
         end,
         offsets = {
           {
