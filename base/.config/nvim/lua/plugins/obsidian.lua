f9aa207ba2d4500bf11eb6f869b641de81083e6c
Chad Nickell <nickell.chad@gmail.com>
2025-01-07 13:26:25 -0500

    Obsidian frontmatter

--- base/.config/nvim/lua/plugins/obsidian.lua
+++ base/.config/nvim/lua/plugins/obsidian.lua
@@ -71,7 +71,23 @@ return {
         path = '/home/chad/Documents/notes/Notes',
       },
     },
-    disable_frontmatter = true,
+    -- disable_frontmatter = true,
+    note_frontmatter_func = function(note)
+      -- Add the title of the note as an alias.
+      if note.title then note:add_alias(note.title) end
+
+      local out = { created = os.date '%Y-%m-%d' }
+
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+
+      return out
+    end,
     ui = {
       checkboxes = {
         [' '] = { char = '☐', hl_group = 'ObsidianTodo' },
