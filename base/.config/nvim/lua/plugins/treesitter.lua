93fe65eec98fa4d2d06b11243f8754ac8c85441c
Chad Nickell <nickell.chad@gmail.com>
2025-03-06 12:27:30 -0500

    Bring large file filter back in for treesitter

--- base/.config/nvim/lua/plugins/treesitter.lua
+++ base/.config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,15 @@ return {
       local configs = require 'nvim-treesitter.configs'
 
       configs.setup {
-        highlight = { enable = true },
+        highlight = {
+          enable = true,
+          -- Disable slow treesitter highlight for large files
+          disable = function(_, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then return true end
+          end,
+        },
         indent = { enable = true, disable = { 'yaml' } },
         ensure_installed = {
           'bash',
